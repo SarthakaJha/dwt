e0c4acf57da0afec0bed662b9ca1d312bdc33962
SarthakaJha <SarthakaJha@users.noreply.github.com>
2023-06-23 03:52:07 +0200

    Working on the redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {createBrowserRouter ,createRoutesFromElements, Route ,RouterProvider} from 'react-router-dom'
+import store from './store.jsx'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeScreen from './page/HomeScreen.jsx';
 import Login from './page/Login.jsx';
+ import Register from './page/Register.jsx';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
     </Route>
   )
 );
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router}/>
+    </React.StrictMode>
+  </Provider>
+
 )
