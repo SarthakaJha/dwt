9994454b2f8ce35b5b6e3372cc5a3817cbae46d2
SarthakaJha <SarthakaJha@users.noreply.github.com>
2023-06-28 21:57:47 +0200

    started with the frontend

---
+++ BACKEND/utils/Convertor.js
@@ -0,0 +1,27 @@
+import { parse as json2csv } from "json2csv";
+import fs from "fs";
+import path from "path";
+
+const Convertor = (data, filename) => {
+  // Define the fields and field names for the CSV file
+  const fields = ["irradiance", "electricity", "date"];
+  const fieldNames = ["Irradiance", "Electricity", "Date"];
+
+  // Define the output directory and path for the CSV file
+  const outDir = "./backend/public/files";
+  const outPath = path.join(outDir, filename);
+
+  const csvData = json2csv(data, { fields, fieldNames });
+
+  fs.writeFile(outPath, csvData, function (err) {
+    if (err) {
+      console.error("Error writing CSV file:", err);
+    } else {
+      console.log("CSV file generated successfully!");
+    }
+  });
+
+  return outPath;
+};
+
+export default Convertor;
