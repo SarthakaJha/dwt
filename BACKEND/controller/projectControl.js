36aa890635f5933317926d70e6577df82ff9ce50
SarthakaJha <SarthakaJha@users.noreply.github.com>
2023-06-28 19:12:26 +0200

    modifying the files and Api

--- BACKEND/controller/projectControl.js
+++ BACKEND/controller/projectControl.js
@@ -1,6 +1,10 @@
 import asyncHandler from "express-async-handler";
 import Project from "../models/projectModel.js";
-
+import SendMail from "../utils/mailler.js";
+import  checkDuration  from "../utils/date.js";
+import getEnergyData  from "../utils/WeatherData.js"
+import WeatherData from "../utils/WeatherData.js";
+import ModifiedDate from "../utils/date.js"
 // Create a new project
 export const createProject = asyncHandler(async (req, res) => {
   const { name } = req.body;
@@ -95,6 +99,68 @@ export const fetchProject = asyncHandler(async (req, res) => {
 // Generate a report for a project
 export const createProjectReport = asyncHandler(async (req, res) => {
   // Implementation for generating a project report
+  const project = await Project.findById(req.params.id);
+  if (!project) {
+    res.status(404);
+    throw new Error("Project not found");
+  }
+
+  // Check if user allowed to access the project
+  if (req.user.id.toString() !== project.user.toString()) {
+    res.status(401);
+    throw new Error("User not authorized to access this project");
+  }
+
+  // Check if project is already closed
+  if (project.isClosed) {
+    res.status(400);
+    throw new Error(
+      "Project is already closed. You cannot generate any more reports"
+    );
+  }
+
+  const productLocations = await Product.find(
+    { project: project._id, isClosed: false },
+    { name: 1, lat: 1, lon: 1, area: 1 }
+  );
+
+  if (productLocations.length === 0) {
+    throw new Error(
+      "There are no product locations. You cannot generate a report"
+    );
+  }
+
+  // get the energy data for each product over 30 days and generate its CSV file
+  const attachmentList = [];
+  const weatherDataList = await WeatherData(productLocations);
+  weatherDataList.forEach(async (weatherData) => {
+    const filename = weatherData.name + ".csv";
+    const filepath = convertJSONToCSV(weatherData.data, filename);
+    attachmentList.push({ filename: filename, path: filepath });
+
+    // Update product report data and close the product
+    await Product.findByIdAndUpdate(weatherData._id, {
+      dailyReport: weatherData.data,
+      isClosed: true,
+    });
+  });
+
+  // Send email with the files generated to the user
+  const struct_mail = {
+    to: req.user.email,
+    subject: `Energy Report for ${project.name}`,
+    text: `Please find the reports for products in the project ${project.name} in the attachments below`,
+    attachments: attachmentList,
+  };
+  await SendMail(struct_mail);
+
+  // Close the project
+  await Project.findOneAndUpdate(project._id, { isClosed: true });
+
+  res.status(200).json({
+    message:
+      "Report has been generated. Please check your email for more information",
+  });
 });
 
 // module.exports = {
